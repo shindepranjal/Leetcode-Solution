4c2d3d86e3d5e2eb691e51800c2b4f531c45c3af
Pranjali Shinde <97864318+shindepranjal@users.noreply.github.com>
2023-12-20 10:40:50 +0530

    Time: 3 ms (21.51%), Space: 6.2 MB (73.05%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     int addDigits(int num) {
-      if(num == 0) return 0;
-        else if(num % 9 == 0) return 9;
-        else return num % 9;
-    }
-    
+        while(num>9){
+            int ans=0;
+            int rem;
+            while(num!=0){
+                rem=num%10;
+                num =num/10;
+                ans= ans+rem;
+
+            }
+            num =ans;
+
+        }
+        return num;
+     
+     }
+        
+        
 };

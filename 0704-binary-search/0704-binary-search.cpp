b130206c7dc7aa14b271dc616e1aed071080f38d
Pranjali Shinde <97864318+shindepranjal@users.noreply.github.com>
2024-01-24 09:29:43 +0530

    Time: 33 ms (33.43%), Space: 30 MB (5.49%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target) return i;
+    int search(vector<int>& arr, int target) {
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==target) return i;
             
+        // }
+        // return -1;
+        int n=arr.size();
+        int start= 0;
+        int end=n-1;
+        int mid;
+        while(start<=end){
+            mid=(start+end)/2;
+            //mid==target
+            if(arr[mid]==target){
+                return mid;
+            }
+            //mid<target
+            else if(arr[mid]<target){
+                start=mid+1;
+            }
+            //mid>target
+            else
+            {
+                end=mid-1;
+            }
         }
+
         return -1;
         
     }

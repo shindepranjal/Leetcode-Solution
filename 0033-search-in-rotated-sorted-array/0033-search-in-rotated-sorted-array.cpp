133ac1cd372a2ce5ba04e8fabd9adc2e68e8c8d0
Pranjali Shinde <97864318+shindepranjal@users.noreply.github.com>
2024-02-11 13:33:30 +0530

    Time: 8 ms (9.38%), Space: 13.6 MB (14.31%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
     int search(vector<int>& arr, int target) {
-        int ans=-1;
+//         int ans=-1;
+//         int start=0;
+//         int end=arr.size()-1;
+//         while(start<=end){
+//             int mid=start+(end-start)/2;
+//             if(arr[mid]==target){
+//                 return mid;
+//             }
+//             else if(arr[mid]>=arr[0]){
+//                 if(arr[start]<=target && arr[mid]>=target)
+//                 end=mid-1;
+//                 else
+//                 start=mid+1;
+//             }
+//             else{
+//                 //right side sorted
+//                 if(arr[mid]<=target && arr[end]>=target)
+//                  start=mid+1;
+//                  else
+//                  end=mid-1;
+//             }
+//         }
+
+//         return -1;
+        
         int start=0;
         int end=arr.size()-1;
         while(start<=end){
@@ -9,21 +33,19 @@ public:
             if(arr[mid]==target){
                 return mid;
             }
-            else if(arr[mid]>=arr[0]){
-                if(arr[start]<=target && arr[mid]>=target)
-                end=mid-1;
+            else if(arr[0]<=arr[mid]){
+                if(arr[start]<=target && arr[mid]>target)
+                    end=mid-1;
                 else
-                start=mid+1;
+                    start=mid+1;
             }
             else{
-                //right side sorted
-                if(arr[mid]<=target && arr[end]>=target)
-                 start=mid+1;
-                 else
-                 end=mid-1;
+                if(arr[mid]<target && arr[end]>=target)
+                    start=mid+1;
+                else
+                    end=mid-1;
             }
         }
-
         return -1;
     }
 };
